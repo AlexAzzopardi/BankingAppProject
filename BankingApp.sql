6ca9d3d8f0bf71b61b0d18db9437b070cd36df3e
Alex Azzopardi <32036699+AlexAzzopardi@users.noreply.github.com>
2023-03-03 13:22:47 +0000

    Add files via upload

---
+++ BankingApp.sql
@@ -0,0 +1,11 @@
+create database bankingproject;
+
+use bankingproject;
+
+select * from users;
+select * from accounts;
+select * from transactions;
+
+drop table users;
+drop table accounts;
+drop table transactions;
